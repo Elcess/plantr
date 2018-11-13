8b8ab5fa2ecd38de6e24dc09aae0fc4023aa5af5
Elcess <kelcess@alum.mit.edu>
2018-11-12 19:02:17 -0500

    through seeding tables

--- models.js
+++ models.js
@@ -1,36 +1,31 @@
 const Sequelize = require('sequelize');
 const pg = require('pg');
-const db = new Sequelize('postgres://localhost:5432/plantr', {logging: false});
+const db = new Sequelize('postgres://localhost:5432/plantr', {
+  logging: false,
+});
 
 const Gardener = db.define('gardener', {
   name: Sequelize.STRING,
-  age: Sequelize.INTEGER
-
+  age: Sequelize.INTEGER,
 });
 const Plot = db.define('plot', {
-  // name: Sequelize.STRING,
-  // age : Sequelize.INTEGER
   size: Sequelize.INTEGER,
-  shaded: Sequelize.BOOLEAN
-
+  shaded: Sequelize.BOOLEAN,
 });
 
 const Vegetable = db.define('vegetable', {
   name: Sequelize.STRING,
-  // age : Sequelize.INTEGER
   color: Sequelize.STRING,
-  planted_on: Sequelize.DATE
-
+  planted_on: Sequelize.DATE,
 });
 
-Plot.belongsTo(Gardener, {as: 'owner'});
+Plot.belongsTo(Gardener, { as: 'owner' });
 
 Gardener.hasOne(Plot);
 
-Plot.belongsToMany(Vegetable, {through: 'garden'});
-Vegetable.belongsToMany(Plot, {through: 'garden'});
+Plot.belongsToMany(Vegetable, { through: 'garden' });
+Vegetable.belongsToMany(Plot, { through: 'garden' });
 
-Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
-
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
 
 module.exports = db;
--- seed.js
+++ seed.js
@@ -1,11 +1,48 @@
 const db = require('./models');
+const Sequelize = require('sequelize');
 
-let synced = db.sync({force: true})
+// let synced = db.sync({force: true})
+//   .then(() => {
+//     db.close();
+//     })
+//       .catch(error =>
+//         {
+//           console.error(error);
+//           db.close()
+//   });
+
+let veggies = db.models.vegetable
+  .create(
+    //[
+    {
+      name: 'tomato',
+      color: 'red',
+      planted_on: Date.now(),
+    } //,
+    //   {
+    //     name: 'broccoli',
+    //     color: 'green',
+    //     planted_on: Date.now(),
+    //   },
+    //   {
+    //     name: 'cauliflower',
+    //     color: 'white',
+    //     planted_on: Date.now(),
+    //   },
+    // ]
+  )
+  .then(() => {
+    return db.models.plot.create({ size: 30, shaded: true });
+  })
+  // .then(() => {
+  //   return db.models.garden.create({ plotId: id, vegetableId: vegetable.id });
+  // })
+  .then(() => {
+    return db.models.gardener.create({
+      name: 'Mary',
+      age: 25,
+    });
+  })
   .then(() => {
     db.close();
-    })
-      .catch(error =>
-        {
-          console.error(error);
-          db.close()
-        });
+  });
